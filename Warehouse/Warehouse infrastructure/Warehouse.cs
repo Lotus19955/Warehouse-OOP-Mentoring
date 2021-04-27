30ae601b001e62beadaf3d9959cc1a78bad852d7
Lotus19955 <lotus19955@gmail.com>
2021-04-27 18:47:39 +0300

    day 1 (need to do validation and Update info)

---
+++ Warehouse/Warehouse infrastructure/Warehouse.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse_infrastructure
+{
+    public class Warehouse
+    {
+        public string Title { get; set; }
+        public string Address { get; set; }
+        public int ContuctNumber { get; set; }
+
+        public Warehouse(string title, string address, int contuctNumber)
+        {
+            this.Title = title;
+            this.Address = address;
+            this.ContuctNumber = contuctNumber;
+        }
+        /// <summary>
+        /// Display to console warehouse 'title'
+        /// </summary>
+        public void DisplayTitle(Warehouse displayInformation)
+        {
+            Console.WriteLine($"Warehouse title: '{displayInformation.Title}'");
+        }
+        /// <summary>
+        /// Display to console warehouse 'adress'
+        /// </summary>
+        public void DisplayAddress(Warehouse displayInformation)
+        {
+            Console.WriteLine($"Warehouse address: {displayInformation.Address}");
+        }
+        /// <summary>
+        /// Display to console warehouse 'contuct number'
+        /// </summary>
+        public void DisplayContuctNumber(Warehouse displayContuctNumber)
+        {
+            Console.WriteLine($"Warehouse contuct number: {displayContuctNumber.ContuctNumber}");
+        }
+    }
+}
---
+++ Warehouse/Warehouse/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Warehouse_infrastructure;
+
+namespace WarehouseOOPMentoring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Warehouse sklad = AddInformation();
+        }
+        /// <summary>
+        /// Create you own objects
+        /// </summary>
+        /// <returns>object</returns>
+        public static Warehouse AddInformation()
+        {
+            Console.Write("Enter title of waherouse: ");
+            string title = Console.ReadLine();
+
+            Console.Write("Enter address of waherouse: ");
+            string address = Console.ReadLine();
+
+            Console.Write("Enter contuct number of waherouse: ");
+            int contuctNumber = int.Parse(Console.ReadLine());
+
+            Warehouse sklad = new Warehouse(title, address, contuctNumber);
+            return sklad;
+        }
+    }
+}
