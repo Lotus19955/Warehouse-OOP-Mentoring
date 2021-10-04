d6a4ed9acf28ab80e0d1688075eda34277d17343
lotus19955 <lotus19955@gmail.com>
2021-10-04 15:11:08 +0300

    Added task manager

---
+++ TaskManager/Task manager/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_manager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> homeWork = new Queue<string>();
+            homeWork.Enqueue("cooking");
+            homeWork.Enqueue("cleaning");
+            homeWork.Enqueue("washing");
+            homeWork.Enqueue("garden keep");
+            while (homeWork.Count > 0)
+            {
+                TaskSolver(homeWork);
+            } 
+        }
+        public static Queue<string> TaskSolver(Queue<string> task)
+        {
+            task.Dequeue();
+            return task;
+        }
+    }
+}
